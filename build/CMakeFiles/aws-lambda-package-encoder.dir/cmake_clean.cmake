b37e4562348af464d4f33c1914c3a8595f5e6667
stutxo <70952638+stutxo@users.noreply.github.com>
2025-03-10 00:24:53 -0400

    test c++ lambda

---
+++ build/CMakeFiles/aws-lambda-package-encoder.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aws-lambda-package-encoder"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/aws-lambda-package-encoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/aws-lambda-package-encoder.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for aws-lambda-package-encoder.
+# This may be replaced when dependencies are built.
